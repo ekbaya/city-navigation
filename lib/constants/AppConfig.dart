0e96f14c9409e61b62dec3c49853690625298c18
ekbaya <eliasbaya1223@gmail.com>
2022-10-24 08:20:41 +0300

    Updating App

--- lib/constants/AppConfig.dart
+++ lib/constants/AppConfig.dart
@@ -10,4 +10,4 @@ const String accountUrl = "$baseUrl/user/account";
 const String searchTripsPagesUrl = "$baseUrl/navigation/search-trips";
 const String searchStopsPagesUrl = "$baseUrl/navigation/search-stops";
 
-String mapKey = "AIzaSyBGORjYyKEgXaFYcxzx1482WLlsVGHo9x0";
+String mapKey = "AIzaSyBDOjOJB4-uVM2YssqKA0KNOwv8WSRxBd4";
